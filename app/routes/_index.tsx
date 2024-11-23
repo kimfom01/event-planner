29476ee075208621208a45f5bbf2131a3e93637e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-11-23 23:02:03 +0300

    ✏️ Fix typo in landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
         A better way of <span className="italic">Eventing</span>
       </h1>
       <p className="text-xl text-center md:text-left text-slate-400 font-semibold">
-        Try our early beta and never loose track of your notes again!
+        Try our early beta and never loose track of your events again!
       </p>
       <Link
         className="px-6 py-2 bg-slate-700 hover:bg-slate-600 rounded-lg font-bold"
